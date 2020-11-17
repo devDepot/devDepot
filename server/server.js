633c096102d3638d7996686b9bef2b62e2ba7d9d
ymturan <ymturan1@gmail.com>
2020-11-17 10:52:22 -0500

    none

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
-const PORT = 3000;
+const PORT = 3005;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
