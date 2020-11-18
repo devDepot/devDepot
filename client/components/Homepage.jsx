a85fac9c451f46cee67eea399ec90c28b9f1fb5a
ANRAx <ramdassaj@gmail.com>
2020-11-18 10:48:09 -0500

    changed devs to allDevs in homepage

--- client/components/Homepage.jsx
+++ client/components/Homepage.jsx
@@ -3,41 +3,41 @@ import { Link, Redirect, withRouter } from 'react-router-dom';
 import DevCard from './DevCard';
 import Navbar from './Navbar';
 
-// TODO: once dev has been added to cart, prevent 
+// TODO: once dev has been added to cart, prevent
 const addToCart = ({ inCart, setInCart, devSelected, setDevSelected }) => {
   if (devSelected) {
-    alert("This Developer has already been added to your cart!");
+    alert('This Developer has already been added to your cart!');
   }
-
-}
+};
 
 const showAllDevs = ({ devs, setDevs }) => {
-  const devs = [];
+  const allDevs = [];
 
   fetch('/developers')
     .then((res) => res.json())
     .then((data) => {
       setDevs([data.developers]);
-      devs.forEach((dev) => devs.push(<DevCard />));
+      allDevs.forEach((dev) => devs.push(<DevCard />));
     });
 
   return (
     <div>
       <div className="dev-cards">
-        {devs}
+        {allDevs}
         <button onClick={addToCart} />
       </div>
     </div>
   );
 };
 
-
-
 const Homepage = ({ user, history, inCart, setInCart, devSelected, setDevSelected }) => {
   return (
     <div>
       <Navbar />
-        <h1>Hello, {user}</h1>
+      <h1>
+        Hello,
+        {user}
+      </h1>
       <div>
         <showAllDevs />
       </div>
