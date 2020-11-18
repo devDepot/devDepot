09fd261286a52e99eccf1a12fe3ac08d4b26fdea
ANRAx <ramdassaj@gmail.com>
2020-11-18 00:51:42 -0500

    start building out Homepage, DevCard and adding logic for adding user to cart

--- client/components/Homepage.jsx
+++ client/components/Homepage.jsx
@@ -1,16 +1,47 @@
 import React, { useState } from 'react';
 import { Link, Redirect, withRouter } from 'react-router-dom';
+import DevCard from './DevCard';
+import Navbar from './Navbar';
 
-// TODO: "Hello, ${user}" should be dynamic, pass username down as props from App component
+// TODO: once dev has been added to cart, prevent 
+const addToCart = ({ inCart, setInCart, devSelected, setDevSelected }) => {
+  if (devSelected) {
+    alert("This Developer has already been added to your cart!");
+  }
+  
+}
 
-const Homepage = ({ history }) => {
+const showAllDevs = ({ devs, setDevs }) => {
+  const devs = [];
+
+  fetch('/developers')
+    .then((res) => res.json())
+    .then((data) => {
+      setDevs([data.developers]);
+      devs.forEach((dev) => devs.push(<DevCard />));
+    });
+
   return (
     <div>
-      {/* <Navbar /> */}
-      <span>
-        <h1>Hello, User</h1>
-      </span>
-      <div className="dev-card-container"/>
+      <div className="dev-cards">
+        {devs}
+        <button id=onClick={} />
+      </div>
+    </div>
+  );
+};
+
+
+
+const Homepage = ({ user, history, inCart, setInCart, devSelected, setDevSelected }) => {
+  if ()
+  return (
+    <div>
+      <Navbar />
+        <h1>Hello, {user}</h1>
+      <div>
+        <showAllDevs />
+      </div>
     </div>
   );
 };
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,23 +3,23 @@ const DbModel = require('../models/DbModel.js')
 
 const userController = {};
 
-userController./*POST*/ = async (req,res,next) => {
-    try {
+// userController./*POST*/ = async (req,res,next) => {
+//     try {
 
         
-        const username = req.body.username;
-        const password = req.body.password;
-        const email = req.body.email;
-        const session = req.body.session;
-        const userType = req.body.userType;
+//         const username = req.body.username;
+//         const password = req.body.password;
+//         const email = req.body.email;
+//         const session = req.body.session;
+//         const userType = req.body.userType;
 
 
 
-        // const query = `INSERT `
+//         // const query = `INSERT `
 
-    } catch(err) {
-        return next(err);
-    }
-}
+//     } catch(err) {
+//         return next(err);
+//     }
+// }
 
 module.exports = userController;
