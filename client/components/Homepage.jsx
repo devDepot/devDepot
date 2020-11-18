aa8e7d2aa3029b7a8dfc5d8cbb83ab8c6eacaece
Benjamin Brower <bbrower1293@gmail.com>
2020-11-17 20:11:43 -0500

    added state to components

--- client/components/Homepage.jsx
+++ client/components/Homepage.jsx
@@ -3,23 +3,16 @@ import { Link, Redirect, withRouter } from 'react-router-dom';
 
 // TODO: "Hello, ${user}" should be dynamic, pass username down as props from App component
 
-const displayAllDevs = ({ devs, inCart, devSelected, filterOptions, username }) => {
-  
-}
-
 const Homepage = ({ history }) => {
-
   return (
     <div>
       {/* <Navbar /> */}
       <span>
         <h1>Hello, User</h1>
       </span>
-      <div className="dev-card-container">
-
-      </div>
+      <div className="dev-card-container"/>
     </div>
-  )
-}
+  );
+};
 
 export default withRouter(Homepage);
